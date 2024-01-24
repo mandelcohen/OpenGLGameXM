79da061a689d0294cb76cabf09c0fff4820433f0
mandelcohen <mandel.cohen@student.forsbergsskola.se>
2024-01-24 10:33:06 +0100

    First exercise two triangles same vertex

--- OpenGLGameX/main.cpp
+++ OpenGLGameX/main.cpp
@@ -5,24 +5,15 @@
 
 using namespace std;
 
-void processInput(GLFWwindow*);
+void processInput(GLFWwindow*); // forward declaration, defined further down
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    glViewport(0, 0, width, height);
+    }
 
 void error_callback(int error, const char* msg) {
     cerr << " [" << error << "] " << msg << endl;
-}
-
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
+    }
 
 int main() {
 
@@ -58,7 +49,11 @@ int main() {
         glfwTerminate();
         return -1;
     }
-    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
     
 //    initialisation ends here
 //    ========================
@@ -66,64 +61,81 @@ int main() {
     
     float red = 0;
     
-    // ----- Create array buffer and copy our verticies to GPU ------
-    
+    // Create array buffer and copy our verticies to GPU
     float vertices[] = {
         -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f,
+        
+         0.0f, -0.5f, 0.0f,
+         0.9f, -0.5f, 0.0f,
+         0.45f, 0.5f, 0.0f
     };
     
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO; // variables to store the buffer IDs
     glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &VBO); // ask
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
+    
+    
+    // configure vertex attributes, so the shader knows wher to find the position
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glEnableVertexAttribArray(0); 
+    
+    // glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer
     glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
     glBindVertexArray(0);
     
     // build and compile our shader program
     // ------------------------------------
     // vertex shader
+    const char* vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0"; // the souce explains to the GPU where and what information should go out on the screen
+
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
 
     // fragment shader
+    const char* fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\n\0"; // gives further information, finds out wat color each pixel should be.
+    
     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
+    glCompileShader(fragmentShader); // compile the shader on GPU
     
-    // link shaders
+    // link shaders, RENDER PIPELINE shader program
     unsigned int shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
-    // delete them after use
+    // clean up shaders after they've been linked into a program
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
     
     // While the User doesn't want to Quit
     while (!glfwWindowShouldClose(window))
-    {   // handle input (e.g. close window on ESC)
+    {   // process input (e.g. close window on ESC)
         glfwPollEvents();
         processInput(window);
         
+        // add variable for colour red
         red += 0.001f;
         if(red > 1)
             red -= 1;
         
-        // render
+        // render (paint the current fram)
         glClearColor(red, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         // present
@@ -131,14 +143,18 @@ int main() {
         // draw our first triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        // glBindVertexArray(0); // no need to unbind it every time
+        
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        
          
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
+        // present (send the current frame to the computer screen)
         glfwSwapBuffers(window);
-        glfwPollEvents();
+        //glfwPollEvents();
     }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+    
     // Cleans up all the GLFW stuff
     glfwTerminate();
     
