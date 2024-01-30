876249e213aca811a09e322de38085afce846c8f
mandelcohen <mandel.cohen@student.forsbergsskola.se>
2024-01-30 10:33:31 +0100

    Manipualte shader code for exercises 1-3

--- OpenGLGameX/glsl/vertexShader.glsl
+++ OpenGLGameX/glsl/vertexShader.glsl
@@ -1,7 +1,10 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 
+out vec4 colourByPos;
+
 void main()
 {
     gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    colourByPos = vec4(aPos.x, aPos.y, aPos.z, 1.0);
 }
