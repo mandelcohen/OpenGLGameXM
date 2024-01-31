d155265e0c6c8d23148f87797ec2f1ea521e6997
mandelcohen <mandel.cohen@student.forsbergsskola.se>
2024-01-31 13:23:27 +0100

    Add image and load function

--- OpenGLGameX/main.cpp
+++ OpenGLGameX/main.cpp
@@ -6,6 +6,7 @@
 #include "Mesh.h"
 #include "Shader.h"
 #include "Triangle.h"
+#include "stb_image.h"
 
 using namespace std;
 
@@ -14,6 +15,9 @@ using namespace std;
 int main() {
     
     Window window {800, 600};
+    
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
     
     Vertex vertices1[] {
         Vertex{Vector3{-1.0f, -0.5f, 0.0f}},
@@ -74,7 +78,6 @@ int main() {
     b.red = 0; b.green = 1; b.blue = 0; b.offset = 0;
     
     Triangle c {&mesh3, &rainbow};
-    c.offset = 0.5f;
     
 //    Render only outlines
 //    glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
@@ -85,9 +88,9 @@ int main() {
         window.processInput();
         window.clear();
                 
-        a.Render();
-        b.Render();
-//        c.Render();
+//        a.Render();
+//        b.Render();
+        c.Render();
         window.Present();
     }
     
